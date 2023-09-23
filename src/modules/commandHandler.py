7cbeb12d783fd35edc37f5e0373025bbd9786f43
Samuel M <s.montanez@uniandes.edu.co>
2023-09-22 22:27:14 -0500

    Hotfix
    Remake response and fix split

--- src/modules/commandHandler.py
+++ src/modules/commandHandler.py
@@ -1,8 +1,8 @@
 def commandHandler(msgInfo, prefix, commands):
 	message = msgInfo.content
-	words = message.split
+	words = message.split()
 	command_name = words[0][len(prefix):]
 	response = None
 	if len(words) > 1:
-		response = words[1:]
+		response = ' '.join(words[1:])
 	return commands[command_name], response
