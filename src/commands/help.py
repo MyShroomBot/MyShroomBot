9c87a90c27c15f3a54b434bc25bfbf22b7428e49
Samuel M <s.montanez@uniandes.edu.co>
2023-09-22 22:26:26 -0500

    Doc and Hotfixs
    Add __doc__ config and hotfix on some minor functions

--- src/commands/help.py
+++ src/commands/help.py
@@ -1,17 +1,25 @@
+"""
+Command for providing help:
+Gives you assistance with any command or specific command usage. 
+Simply type '!help <command>' and you'll receive the information of the commands.
+"""
+
 from attributes.command_a import command
 
-help_text = '> Ayuda'
+help_text = '> help - Command for providing help. \n > shroom - Command for Analyzing Mushroom Images.'
 error_no_command_text = 'Command not recognized. Type !help for see the commands available.'
 
 @command
 async def help(Client, ctx, command_name):
 	if not command_name:
-		await ctx.channel.send(help_text)
+		await ctx.channel.send(help_text) #for in docstring.strip().split('\n')[0]
 	else:
 		commands = Client.handler.commands
 		list_names = list(commands.keys())
 		if command_name in list_names:
-			doc = commands[command_name].__doc__ #Info del Comando
+			doc = commands[command_name].__doc__
 			await ctx.channel.send(doc)
 		else:
 			await ctx.channel.send(error_no_command_text)
+
+help.__doc__ = __doc__
--- src/commands/imageIdentifier.py
+++ src/commands/imageIdentifier.py
@@ -1,19 +1,22 @@
+"""
+Command for Analyzing Mushroom Images:
+Once invoked, you can submit a photo in one of the supported formats (PNG, JPG, JPEG, TIFF, and BMP) with a maximum file size of 10MB.
+Subsequently, you will receive a response listing the five most likely mushroom species, organized in descending order of probability.
+"""
+
 from attributes.command_a import command
 from attributes.rename_a import rename
-
-from PIL import Image
-import os
 
 valid_types = ('.png', '.jpg', '.jpeg', '.tiff', '.bmp')
 image_text = 'Hello! Send me the image you wish me to analize.'
 error_noImage = 'Sorry, you did not attach an Image or use a format I do not support.'
-error_noValid = 'Sorry, you use a no valid format of Image. Supported types: png, jpg, jpeg, tiff and bmp'
+error_noValid = 'Sorry, you use a no valid format of Image. Supported types: PNG, JPG, JPEG, TIFF, and BMP'
 error_noSize = 'Sorry, your file is above the valid size. Maximum file size: 10MB'
 
 @rename('shroom')
 @command
-async def imageIdentifier(Client, ctx, command_name):
-    await ctx.send(image_text)
+async def imageIdentifier(Client, ctx, extra):
+    await ctx.channel.send(image_text)
     def checkerResponse(m):
         return m.attachments and m.channel == ctx.channel and m.author == ctx.author
     response = await Client.wait_for('message', timeout=30.0, check=checkerResponse)
@@ -31,4 +34,11 @@ async def imageIdentifier(Client, ctx, command_name):
         await ctx.channel.send(error_noSize)
         return
     
-    attachment.url #Conexión base de datos
+    probs = model.analize(attachment.url) #Conexión base de datos
+
+    answer = ''
+    for prob in probs:
+        answer += f'> {prob[0]} \n'
+    await ctx.channel.send(answer)
+
+imageIdentifier.__doc__ = __doc__
