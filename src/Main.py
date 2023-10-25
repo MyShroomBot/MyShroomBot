ea53cf40071a66d4f2b0b1a5d06845803aeafab7
JuanRuedz <jd.ruedat@uniandes.edu.co>
2023-10-25 00:18:27 -0500

    Cambie la forma en que se obtiene el token, dotenv

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,8 @@
 from classes.Client import BotClient
 import os
+from dotenv import load_dotenv
 
 Bot = BotClient()
-Token = 'MTE1NzE4MTU2NTQ3OTE3MDA2OA.Gz53wQ.DlGufppz9Ye7SiUN6Vm6N0TmkAnCdjiBerkj7I'
-Bot.run(Token)
+load_dotenv()
+
+Bot.run(os.getenv('Token',''))
